224a9410ab0df31862e8fa3e01344e0f0bde0af4
Apurv Singh <rkumarsingh003@gmail.com>
2026-04-16 14:52:24 +0530

    add pages

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import CartPage from "./Pages/CartPage";
 import ProductPaga from "./Pages/ProductPaga";
+import Checkout from "./Pages/Checkout";
+import OrderSuccess from "./Pages/OrderSuccess";
 import Context from "./ContextApi/Context";
 
 function App() {
@@ -15,6 +18,8 @@ function App() {
           <Route path="/" index element={<Home />} />
           <Route path="/CartPage" element={<CartPage />} />
           <Route path={`/ProductPaga/:id`} element={<ProductPaga />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/OrderSuccess/:orderId" element={<OrderSuccess />} />
         </Routes>
         <Footer />
       </Context>
@@ -23,5 +28,3 @@ function App() {
 }
 
 export default App;
-
-CartPage
