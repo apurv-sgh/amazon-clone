feec57d1d3b37583e1616e219317423295f205b5
Apurv Singh <rkumarsingh003@gmail.com>
2026-04-16 14:42:45 +0530

    Fixing vite.config

--- amazon/vite.config.js
+++ amazon/vite.config.js
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  ],
+  plugins: [react(), 
+    tailwindcss()],
   build: {
     // Sets the warning limit to 1MB
     chunkSizeWarningLimit: 1000,
-  }
+  },
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:5000/api'),
+  },
 })
