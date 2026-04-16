ede35880a23e77afa784d708f594809148101852
Apurv Singh <rkumarsingh003@gmail.com>
2026-04-16 22:22:38 +0530

    mysql DB integration

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'amazon_clone',
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
